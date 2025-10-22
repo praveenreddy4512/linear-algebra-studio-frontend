7dd1033c92b681d135a134d972b43f101c803864
KOLLI SAI PRAVEEN REDDY <108921180+praveenreddy4512@users.noreply.github.com>
2025-10-22 13:39:51 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://linear-algebra-studio-backend.vercel.app/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
